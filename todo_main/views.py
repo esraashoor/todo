838679f5a7360196b83814e130ba906c4ccc4891
esra <esra.ashoor.e@gmail.com>
2024-12-07 20:43:40 +0300

    add task functionalities

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,6 +1,11 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-
+from todo.models import Task
 def home(request):
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    # print(tasks)
+    context = {
+        'tasks': tasks,
+    }
     # return HttpResponse('<h1>This is my home page</h1>')
-    return render(request, 'home.html')
+    return render(request, 'home.html', context)
