4c7800bd406ea252925e308db926b1c63f970189
esra <esra.ashoor.e@gmail.com>
2024-12-08 12:16:07 +0300

    done

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed=True)
+    print(completed_tasks)
+
     # print(tasks)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     # return HttpResponse('<h1>This is my home page</h1>')
     return render(request, 'home.html', context)
